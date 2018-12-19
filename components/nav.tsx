01de53159e94fb36b4e0ce7c6e889f4c6ab41a60
hungtsou <hungtsougg@gmail.com>
2018-12-19 02:32:41 -0600

    Fixed prettier formating

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const links = [
-  {
-    href: "https://github.com/segmentio/create-next-app",
-    label: "Github",
-    key: ""
-  }
+	{
+		href: 'https://github.com/segmentio/create-next-app',
+		label: 'Github',
+		key: ''
+	}
 ].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
+	link.key = `nav-link-${link.href}-${link.label}`;
+	return link;
 });
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link prefetch={true} href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </ul>
-  </nav>
+	<nav>
+		<ul>
+			<li>
+				<Link prefetch={true} href="/">
+					<a>Home</a>
+				</Link>
+			</li>
+			<ul>
+				{links.map(({ key, href, label }) => (
+					<li key={key}>
+						<Link href={href}>
+							<a>{label}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</ul>
+	</nav>
 );
 
 export default Nav;
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withTypescript = require("@zeit/next-typescript");
+const withTypescript = require('@zeit/next-typescript');
 
 module.exports = withTypescript({
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: "empty"
-    };
+	webpack: config => {
+		// Fixes npm packages that depend on `fs` module
+		config.node = {
+			fs: 'empty'
+		};
 
-    return config;
-  }
+		return config;
+	}
 });
