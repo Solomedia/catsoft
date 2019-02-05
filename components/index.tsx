255e08bb1c6d1b45c3d982782dacb78062163026
hungtsou <hungtsougg@gmail.com>
2019-02-04 22:37:10 -0600

    Update components exports, update components file names

--- components/index.tsx
+++ components/index.tsx
@@ -1,1 +1,8 @@
 export { default as BannerWithTabs } from './BannerWithTabs';
+export { default as BrandsBannerMob } from './BrandsBannerMob';
+export { default as ReviewSlider } from './ReviewSlider';
+export { default as Reviews } from './Reviews';
+export { default as HeroWithCta } from './HeroWithCta';
+export { default as Head } from './Head';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
