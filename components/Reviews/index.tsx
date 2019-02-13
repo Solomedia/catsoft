dc9579f8bbdd2d77643f63fd4424a0c78d756ffc
josuegb <josuegb15@gmail.com>
2019-02-13 03:48:55 -0600

    Caps name paths error fix - 2

---
+++ components/Reviews/index.tsx
@@ -0,0 +1,47 @@
+import { Flex, Box } from '@rebass/grid/emotion';
+import { css } from '@emotion/core';
+// Local modules
+import { breakpoints } from '../../core/theme';
+import Rate from './rate';
+// TODO: fetch data from API.
+import data from '../../static/mockdata.json';
+
+const Reviews = () => {
+	const { reviews } = data;
+	return (
+		<Box>
+			<Box
+				as="h3"
+				fontSize={[6, 8]}
+				css={css`
+					text-align: center;
+					font-weight: 300;
+				`}
+			>
+				What costumers are saying about us
+			</Box>
+			<Flex
+				mt={6}
+				justifyContent="space-between"
+				css={css`
+					@media (max-width: ${breakpoints['sm']}) {
+						flex-wrap: wrap;
+						max-width: 335px;
+						margin: auto;
+					}
+				`}
+			>
+				{data &&
+					reviews.map((review, index) => (
+						<Rate
+							open={index === 0 ? true : false}
+							key={review.id}
+							data={review}
+						/>
+					))}
+			</Flex>
+		</Box>
+	);
+};
+
+export default Reviews;
