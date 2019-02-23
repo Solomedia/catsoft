723e9d7e7a8cddce4bdc155c61f8459322c11319
hungtsou <hungtsougg@gmail.com>
2019-02-23 02:30:38 -0600

    Create suggest-products-list component

--- components/SuggestProductsList.tsx
+++ components/SuggestProductsList.tsx
@@ -1,15 +1,30 @@
-import { Box } from '@rebass/grid/emotion';
 import { AddProduct } from './';
+import { Text, Row, Col } from 'lib/ui';
 
 interface Props {
   handleAddProduct: (target: HTMLInputElement) => any;
+  data: any;
 }
 
-const SuggestProductsList: React.SFC<Props> = ({ handleAddProduct }) => (
-  <Box>
-    Any suggestion bro
-    <AddProduct handleAddProduct={({ target }) => handleAddProduct(target)} />
-  </Box>
+const SuggestProductsList: React.SFC<Props> = ({
+  data: products,
+  handleAddProduct
+}) => (
+  <Row flexWrap="wrap">
+    <Text weigth="500" px={2} width={1} mt={6}>
+      Get fully protected with these suggestions
+    </Text>
+    {products &&
+      products.map(product => (
+        <Col mt={5} key={product.item_id} width={1 / 3}>
+          <AddProduct
+            data={product}
+            key={product.item_id}
+            handleAddProduct={({ target }) => handleAddProduct(target)}
+          />
+        </Col>
+      ))}
+  </Row>
 );
 
 export default SuggestProductsList;
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -3,6 +3,9 @@ import Main from 'layouts/main';
 import { Container, Text } from 'lib/ui';
 import { CartList, SuggestProductsList } from 'components';
 import mockData from 'static/mockdata.json';
+import { css } from '@emotion/core';
+import { breakpoints } from 'lib/theme';
+import { Box } from '@rebass/grid/emotion';
 
 class Cart extends React.Component<{}> {
   public static async getInitialProps() {
@@ -23,9 +26,18 @@ class Cart extends React.Component<{}> {
             currency={mockData.carts_mine.currency.global_currency_code}
             data={mockData.carts_mine.items}
           />
-          <SuggestProductsList
-            handleAddProduct={target => console.log(target.checked)}
-          />
+          <Box
+            css={css`
+              @media (max-width: ${breakpoints['sm']}) {
+                display: none;
+              }
+            `}
+          >
+            <SuggestProductsList
+              data={mockData.suggest_products_list}
+              handleAddProduct={target => console.log(target.checked)}
+            />
+          </Box>
         </Container>
       </Main>
     );
