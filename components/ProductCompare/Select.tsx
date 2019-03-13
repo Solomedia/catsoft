b02ba60ba4ed74e4934cda9bd08510d991a336ef
josuegb <josuegb15@gmail.com>
2019-03-13 07:56:25 -0600

    Product Compare Select

---
+++ components/ProductCompare/Select.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import Dropdown from 'react-dropdown';
+import styled from 'lib/theme';
+import { css } from '@emotion/core';
+
+interface State {
+  selected: string;
+}
+
+interface Props {
+  selectPosition: number;
+  options: string[];
+  onSelectChange: (selectPosition: number, option: string) => void;
+  placeholder: string;
+}
+
+class Select extends React.Component<Props, State> {
+  public state = {
+    selected: ''
+  };
+
+  private onSelect = option => {
+    const { selectPosition, onSelectChange } = this.props;
+    this.setState({ selected: option });
+    onSelectChange(selectPosition, option.value);
+  };
+
+  public render() {
+    const { options, placeholder } = this.props;
+    const defaultOption = this.state.selected;
+    const arrowClosed = (
+      <Icon
+        className="material-icons"
+        css={css`
+          transform: ${!defaultOption ? 'rotate(45deg)' : 'none'};
+          font-size: ${!defaultOption ? '18px' : '24px'};
+        `}
+      >
+        {defaultOption !== '' ? 'expand_more' : 'close'}
+      </Icon>
+    );
+    const arrowOpen = <Icon className="material-icons">expand_less</Icon>;
+
+    return (
+      <DropdownStyled
+        options={options}
+        onChange={this.onSelect}
+        value={defaultOption}
+        arrowClosed={arrowClosed}
+        arrowOpen={arrowOpen}
+        placeholder={placeholder}
+      />
+    );
+  }
+}
+
+const DropdownStyled = styled(Dropdown)`
+  height: 44px;
+  width: 100%;
+  border: ${({ value }) => (value !== '' ? '0' : '1px solid #d4d5e2')};
+  border-bottom: 1px solid #dbe1e7;
+  background-color: #fff;
+  position: relative;
+  &:hover {
+    cursor: pointer;
+  }
+
+  .Dropdown-control {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    align-items: center;
+    padding: ${({ value }) => (value !== '' ? '16px 4px 16px 0' : '16px')};
+  }
+  .Dropdown-placeholder {
+    color: ${({ value }) => (value !== '' ? '#212B36' : '#6d5cff')};
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: -1px;
+  }
+  .Dropdown-menu {
+    background-color: #fff;
+    border: 1px solid #d4d5e2;
+    width: 100%;
+    padding: 0 10px 10px;
+  }
+  .Dropdown-option {
+    color: #727272;
+    font-size: 14px;
+    line-height: 18px;
+    text-transform: uppercase;
+    margin-top: 10px;
+  }
+`;
+
+const Icon = styled.i`
+  color: #6d5cff;
+  font-weight: bold;
+`;
+
+export default Select;
--- components/ProductCompare/index.tsx
+++ components/ProductCompare/index.tsx
@@ -1,27 +1,105 @@
 import React from 'react';
-import { Text } from 'lib/ui';
+import { Text, Row, Col } from 'lib/ui';
+import Select from './Select';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stars: number;
+  license_use: string;
+  license_type: string;
+  included_products: string[];
+}
+
+interface Products {
+  items: Product[];
+}
 
 interface Category {
   name: string;
+  products: Products;
 }
 
 interface Props {
   categoryToCompare: Category;
 }
+interface State {
+  productsToCompare: Product[];
+  activeProducts?: Product[];
+}
 
-const ProductCompare: React.SFC<Props> = ({ categoryToCompare }) => {
-  const name = categoryToCompare && categoryToCompare.name;
-  return (
-    <>
-      <Text as="h1" mt={8} fontSize={10} weight={300} align="center">
-        Compare {name} Products
-      </Text>
-      <Text mt={3} weight={500} align="center">
-        Select the softwares below that you want to compare. You can add up to 4
-        softwares.
-      </Text>
-    </>
-  );
-};
+class ProductCompare extends React.Component<Props, State> {
+  public state = {
+    productsToCompare: [],
+    activeProducts: []
+  };
+
+  private onSelectChange = (selectPosition: number, option: string) => {
+    const { productsToCompare, activeProducts } = this.state;
+    activeProducts[selectPosition] = productsToCompare.find(({ name }) =>
+      name.includes(option)
+    );
+    this.setState({ activeProducts });
+  };
+
+  public componentDidUpdate() {
+    const { productsToCompare } = this.state;
+
+    if (!productsToCompare.length) {
+      const { categoryToCompare } = this.props;
+      const products = categoryToCompare && categoryToCompare.products;
+      const items = products && products.items;
+      this.setState({ productsToCompare: items });
+    }
+  }
+
+  public render() {
+    const { categoryToCompare } = this.props;
+    const { activeProducts, productsToCompare } = this.state;
+    const categoryName = categoryToCompare && categoryToCompare.name;
+    const options = productsToCompare.reduce((result, { name }) => {
+      const productExist = activeProducts.some(prod => prod.name === name);
+      if (!productExist) result.push(name.replace(/Microsoft Office /g, ''));
+      return result;
+    }, []);
+
+    return (
+      <>
+        <Text as="h1" mt={8} fontSize={10} weight={300} align="center">
+          Compare {categoryName} Products
+        </Text>
+        <Text mt={3} weight={500} align="center">
+          Select the softwares below that you want to compare. You can add up to
+          4 softwares.
+        </Text>
+        <Row justifyContent="center" mt={7}>
+          <Col width={[1 / 4]}>
+            <Select
+              options={options}
+              selectPosition={0}
+              onSelectChange={this.onSelectChange}
+              placeholder="Add one"
+            />
+          </Col>
+          {activeProducts.map(
+            (_, index) =>
+              index > -1 &&
+              index < 3 && (
+                <Col key={index} width={[1 / 4]}>
+                  <Select
+                    selectPosition={index + 1}
+                    options={options}
+                    onSelectChange={this.onSelectChange}
+                    placeholder="Add another"
+                  />
+                </Col>
+              )
+          )}
+        </Row>
+      </>
+    );
+  }
+}
 
 export default ProductCompare;
