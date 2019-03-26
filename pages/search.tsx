387d8a6dd1f390c49843f6460e2ced4cea470470
hungtsou <hungtsougg@gmail.com>
2019-03-26 00:45:47 -0600

    pass api data to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NextFunctionComponent } from 'next';
 import { Container, Text } from 'lib/ui';
 import { ProductsList, SelectFilter } from 'components';
-import mockData from 'static/mockdata.json';
 import { css } from '@emotion/core';
 import styled, { colors } from 'lib/theme';
 import { Flex, Box } from '@rebass/grid/emotion';
@@ -11,46 +10,50 @@ import { searchProductsByName } from 'lib/services/productsService';
 
 const { textColor2, textColor4 } = colors;
 
-const Search: NextFunctionComponent<any> = () => (
-  <>
-    <Container>
-      <Flex
-        mt={4}
-        flexDirection={['column', 'row']}
-        justifyContent="space-between"
-      >
-        <Box>
-          <Text color="#727272">
-            Showing{' '}
-            <span
-              css={css`
-                font-weight: 600;
-              `}
-            >
-              {mockData.products_list && mockData.products_list.length}
-            </span>{' '}
-            search results for:
-          </Text>
-          <Text weight="300" fontSize={10}>
-            <QuoteStyled>“</QuoteStyled>
-            Office
-            <QuoteStyled>”</QuoteStyled>
-          </Text>
-        </Box>
-        <Flex alignItems="center" mt={[1, '0']}>
-          <Text mr={1} color={textColor2}>
-            Sort by
-          </Text>
-          <SelectFilter options={['best selling', 'refine']} />
+const Search: NextFunctionComponent<any> = ({ pageProps }) => {
+  const { data: products, searchValue } = pageProps;
+
+  function content() {
+    if (!products) return <Text>Loading...</Text>;
+
+    return (
+      <>
+        <Flex
+          mt={4}
+          flexDirection={['column', 'row']}
+          justifyContent="space-between"
+        >
+          <Box>
+            <Text color="#727272">
+              Showing{' '}
+              <span
+                css={css`
+                  font-weight: 600;
+                `}
+              >
+                {products.items.length}
+              </span>{' '}
+              search results for:
+            </Text>
+            <Text weight="300" fontSize={10}>
+              <QuoteStyled>“</QuoteStyled>
+              {searchValue}
+              <QuoteStyled>”</QuoteStyled>
+            </Text>
+          </Box>
+          <Flex alignItems="center" mt={[1, '0']}>
+            <Text mr={1} color={textColor2}>
+              Sort by
+            </Text>
+            <SelectFilter options={['best selling', 'refine']} />
+          </Flex>
         </Flex>
-      </Flex>
-      <ProductsList
-        grid={[1, 1 / 2, 1 / 3, 1 / 4]}
-        data={mockData.products_list}
-      />
-    </Container>
-  </>
-);
+        <ProductsList grid={[1, 1 / 2, 1 / 3, 1 / 4]} data={products.items} />
+      </>
+    );
+  }
+  return <Container>{content()}</Container>;
+};
 
 const QuoteStyled = styled.span`
   font-weight: 600;
@@ -58,11 +61,12 @@ const QuoteStyled = styled.span`
 `;
 
 Search.getInitialProps = async ctx => {
-  const data = await searchProductsByName(ctx.query.value, 6, 1);
-  // TODO: pass data to components
-  console.log(data);
+  const searchParam = String(ctx.query.value).replace(/ /g, '%20');
+  const data = await searchProductsByName(searchParam, 99, 1);
+
   return {
-    data
+    data,
+    searchValue: ctx.query.value
   };
 };
 
